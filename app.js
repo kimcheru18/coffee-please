f26b5cfc9f3309e5e9c8a4ec9ffdde689b308baf
kimcheru18@gmail.com <kimcheru18@gmail.com>
2017-09-04 08:56:43 -0700

    updated youtube layout, fixed coffee and location titles

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var coffeeArray = [
     {
         coffeeName: 'Americano',
         coffeeDescription: 'This drink consists of a single or double-shot of espresso combined with up to four or five ounces of hot water in a two-demitasse cup.',
-        coffeeLocation: 'United States',
+        coffeeLocation: 'USA',
         coffeeDetails: "",
         coffeeImage: 'americano.jpg'
 
@@ -26,7 +26,7 @@ var coffeeArray = [
     {
         coffeeName: 'Bicerin',
         coffeeDescription: 'Bicerin is a traditional warm coffee concoction native to Turin, Italy, made of espresso, drinking chocolate and whole milk served layered in a small rounded glass.',
-        coffeeLocation: 'Turin, Italy',
+        coffeeLocation: 'Italy',
         coffeeDetails: "",
         coffeeImage: 'bicerin.jpg'
     },
@@ -35,14 +35,14 @@ var coffeeArray = [
     {
         coffeeName: 'Breve',
         coffeeDescription: 'Caffe Breve is an American variation of a latte: a milk-based espresso drink using steamed half-and-half mixture of milk and cream instead of milk.',
-        coffeeLocation: 'United States',
+        coffeeLocation: 'USA',
         coffeeDetails: "",
         coffeeImage: 'caffe_breve.jpg'
     },
 
     //Coffee 5
     {
-        coffeeName: 'Café Au Lait',
+        coffeeName: 'Café-Au-Lait',
         coffeeDescription: 'Café au lait is a French/European coffee drink and consists of strong or bold coffee (sometimes espresso) mixed with scalded milk in approximately a 1:1 ratio.',
         coffeeLocation: 'Europe',
         coffeeDetails: "",
@@ -51,16 +51,16 @@ var coffeeArray = [
 
     //Coffee 6
     {
-        coffeeName: 'Café Bombón',
+        coffeeName: 'Café-Bombón',
         coffeeDescription: 'A café bombón, uses espresso served with sweetened condensed milk in a 1:1 ratio.',
-        coffeeLocation: 'Valencia, Spain',
+        coffeeLocation: 'Spain',
         coffeeDetails: "",
         coffeeImage: 'cafe_bombon.jpg'
     },
 
     //Coffee 7
     {
-        coffeeName: 'Caffè Corretto',
+        coffeeName: 'Caffè-Corretto',
         coffeeDescription: 'Caffè corretto is an Italian beverage that consists of a shot of espresso with a shot of liquor, usually grappa, and sometimes sambuca or brandy.',
         coffeeLocation: 'Italy',
         coffeeDetails: "",
@@ -69,25 +69,25 @@ var coffeeArray = [
 
     //Coffee 9
     {
-        coffeeName: 'Cafè Crema',
+        coffeeName: 'Cafè-Crema',
         coffeeDescription: 'The term "cafè crema" also refers to a long espresso drink, which consists of a running 180 ml–240 ml (6–8 oz) of water when brewing an espresso, primarily by using a coarser grind.',
-        coffeeLocation: 'Switzerland, Austria and Northern Italy',
+        coffeeLocation: 'Switzerland',
         coffeeDetails: "",
         coffeeImage: 'caffe_crema.jpg'
     },
 
     //Coffee 10
     {
-        coffeeName: 'Café Mélange',
+        coffeeName: 'Café-Mélange',
         coffeeDescription: 'Café mélange is a black coffee mixed (french "mélange") or covered with whipped cream.',
-        coffeeLocation: 'Austria, Switzerland and the Netherlands',
+        coffeeLocation: 'Austria',
         coffeeDetails: "",
         coffeeImage: 'cafe_melange.jpg'
     },
 
     //Coffee 11
     {
-        coffeeName: 'Ca Phe Sua Da',
+        coffeeName: 'Ca-Phe-Sua-Da',
         coffeeDescription: 'Ca phe sua da is made with finely ground Vietnamese-grown dark roast coffee individually brewed with a small metal French drip filter (cà phê phin) into a cup containing about a quarter to a half as much sweetened condensed milk, stirred and poured over ice.',
         coffeeLocation: 'Vietnam',
         coffeeDetails: "",
@@ -123,18 +123,18 @@ var coffeeArray = [
 
     //Coffee 14
     {
-        coffeeName: 'Greek Frappé Coffee',
+        coffeeName: 'Greek-Frappé-Coffee',
         coffeeDescription: 'Frappé coffee also known as Greek frappé is a foam-covered iced coffee drink made from instant coffee (generally, spray-dried).',
-        coffeeLocation: 'Greece and Cyprus',
+        coffeeLocation: 'Greece',
         coffeeDetails: "",
         coffeeImage: 'sanfrappe.jpg'
     },
 
     //Coffee 15
     {
-        coffeeName: 'Indian Filter Coffee',
+        coffeeName: 'Indian-Filter-Coffee',
         coffeeDescription: 'South Indian Coffee, also known as Filter Coffee is a sweet milky coffee made from dark roasted coffee beans (70%-80%) and chicory (20%-30%).',
-        coffeeLocation: 'Andhra Pradesh, Karnataka, Kerala and Tamil Nadu',
+        coffeeLocation: 'Karnataka',
         coffeeDetails: "",
         coffeeImage: 'indian_filter_coffee.jpg'
     },
@@ -142,7 +142,7 @@ var coffeeArray = [
 
     //Coffee 16
     {
-        coffeeName: 'Irish Coffee',
+        coffeeName: 'Irish-Coffee',
         coffeeDescription: 'Irish coffee (Irish: Caife Gaelach) is a cocktail consisting of hot coffee, Irish whiskey, and sugar, stirred, and topped with thick cream.',
         coffeeLocation: 'Ireland',
         coffeeDetails: "",
@@ -151,27 +151,27 @@ var coffeeArray = [
 
     //Coffee 17
     {
-        coffeeName: 'Kopi Tubruk',
+        coffeeName: 'Kopi-Tubruk',
         coffeeDescription: 'Kopi Tubruk is an Indonesian-style coffee where coarse coffee grounds are boiled along with solid sugar, resulting in a thick drink similar to Turkish coffee. ',
-        coffeeLocation: 'Bali and Java',
+        coffeeLocation: 'Bali',
         coffeeDetails: "",
         coffeeImage: 'kopi_tubruk.jpg'
     },
 
     //Coffee 18
     {
-        coffeeName: 'Turkish Coffee',
+        coffeeName: 'Turkish-Coffee',
         coffeeDescription: 'Turkish Coffee prepared by boiling very finely powdered roast coffee beans two or three times in a pot (cezve), possibly with sugar, and serving it into a cup, where the dregs settle. It has a foam on top, similar to the crema on espresso.',
-        coffeeLocation: 'Middle East, North Africa, Caucasus, and the Balkans',
+        coffeeLocation: 'Turkey',
         coffeeDetails: "",
         coffeeImage: 'turkishcoffee.jpg'
     },
 
     //Coffee 19
     {
-        coffeeName: 'Vienna Coffee',
+        coffeeName: 'Vienna-Coffee',
         coffeeDescription: 'Vienna Coffee is made by preparing two shots of strong black espresso and infusing the coffee with whipped cream until the cup is full. Then the cream is twirled and topped off with chocolate sprinklings. It is drunk through the creamy top.',
-        coffeeLocation: 'Vienna',
+        coffeeLocation: 'Austria',
         coffeeDetails: "",
         coffeeImage: 'vienna.jpg'
     },
@@ -180,7 +180,7 @@ var coffeeArray = [
     {
         coffeeName: 'Yuanyang',
         coffeeDescription: 'Yuanyang or "coffee with tea" is a popular beverage in Hong Kong, made of a mixture of coffee and Hong Kong-style milk tea.',
-        coffeeLocation: 'Hong Kong and Macau',
+        coffeeLocation: 'Macau',
         coffeeDetails: "",
         coffeeImage: 'yuanyang.jpg'
     },
@@ -191,7 +191,7 @@ var coffeeArray = [
 function displayCoffeeTypes(coffeeArray) {
     var buildTheHtmlOutput = "";
     $.each(coffeeArray, function (coffeeArrayKey, coffeeArrayValue) {
-        buildTheHtmlOutput += "<li class='slide_open' onclick=updateSlideContent('" + coffeeArrayValue.coffeeName + '-' + coffeeArrayValue.coffeeLocation + "')>";
+        buildTheHtmlOutput += "<li class='slide_open' onclick=updateSlideContent('" + coffeeArrayValue.coffeeName + '_' + coffeeArrayValue.coffeeLocation + "')>";
         buildTheHtmlOutput += '<div class="coffee-image" style="background-image: url(images/' + coffeeArrayValue.coffeeImage + ')"></div>';
         buildTheHtmlOutput += '<div class="coffee-name">';
         buildTheHtmlOutput += coffeeArrayValue.coffeeName;
@@ -209,21 +209,21 @@ function displayCoffeeTypes(coffeeArray) {
 //step 3. dynamically created layout to display overlay
 
 function updateSlideContent(coffeeDetails) {
-    var oneDetail = coffeeDetails.split("-");
+    var oneDetail = coffeeDetails.split("_");
 
     //display general data
     $("#slide h4").html(oneDetail[0]);
     $("#slide p.google-map").html(oneDetail[1]);
 
     //display the map data
-    var mapOutput = '<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDlI_svxqzPA4F944kcyUEnC6-roh51lfc &q=' + oneDetail[1] + '" allowfullscreen></iframe>';
+    var mapOutput = '<iframe width="100%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDlI_svxqzPA4F944kcyUEnC6-roh51lfc &q=' + oneDetail[1] + '" allowfullscreen></iframe>';
     $("#slide .google-map-wrapper").html(mapOutput);
 
     //    display wikipedia data
     //    $("#slide .wiki-wrapper").html(wikiOutput);
 
     //    display youtube data
-    getYoutubeResults(`${oneDetail[0]} coffee`);
+    getYoutubeResults(oneDetail[0]);
 
 }
 
@@ -233,9 +233,9 @@ function getYoutubeResults(makeCoffee) {
     $.getJSON("https://www.googleapis.com/youtube/v3/search", {
             part: "snippet",
             key: "AIzaSyBoG71V0P4gwy5i7TvDPV9JSJAdokte3NI",
-            maxResults: 4,
+            maxResults: 2,
             type: "video",
-            q: makeCoffee
+            q: "how to make" + makeCoffee
         },
 
         function (receivedApiData) {
